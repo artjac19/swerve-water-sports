4a7d26649cff2b71bc59b5dee9ac562b59ab5c6e
artjac19 <67008854+artjac19@users.noreply.github.com>
2024-07-11 20:38:11 -0500

    initial commit

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<template>
+  <div class="vert-container">
+    <h2>Contact</h2>
+    <div class="photos" style="margin-bottom: 1.5vh;">
+    <a href="https://www.youtube.com/@swervewatersports" target="_blank" style="position: relative; display: inline-block;">
+        <img src="../assets/youtubelogo.png" width="30px" height="30px" style="margin-right:3vh;">
+        <span class="link-overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></span>
+    </a>
+    
+    <a href="https://www.instagram.com/swervewatersportscenter" target="_blank" style="position: relative; display: inline-block;">
+        <img src="../assets/instagramlogo.png" width="30px" height="30px" style="margin-right:3vh;">
+        <span class="link-overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></span>
+    </a>
+    
+    <a href="https://www.facebook.com/SwerveWatersports" target="_blank" style="position: relative; display: inline-block;">
+        <img src="../assets/facebooklogo.png" width="30px" height="30px" style="margin-right:3vh;">
+        <span class="link-overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></span>
+    </a>
+    
+    <a href="https://www.tiktok.com/@Swervewatersports" target="_blank" style="position: relative; display: inline-block;">
+        <img src="../assets/tiktoklogo.png" width="30px" height="30px">
+        <span class="link-overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></span>
+    </a>
+</div>
+
+
+    <p style="align-self: center; text-align: center; min-width: 50vh; margin-top: 0;">608 Love Lane Road, Hillsboro, TN 36342
+      <br>
+      mike@swerveboards.com
+      <br>
+      (303) 817 - 0806
+    </p>
+  </div>
+</template>
